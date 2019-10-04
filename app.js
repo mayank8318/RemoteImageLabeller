e6c4d3dc4b5cf100091fb2d4e8df9162c5160b81
Mayank Vaidya <mayank8318@gmail.com>
2019-10-04 15:36:13 +0530

    Initialize app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+const  env_vars = require('./Utils/Constants.js') ;
+const  express = require('express');
+const body_parser = require('body-parser');
+const _ = require('lodash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const logger = require('morgan');
+const  passport_config = require('./config/passport_config');
+var flash = require('connect-flash');
+
+let user = require("./routes/user");
+
+let app = express();
+app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.json());
+app.use(logger('dev'));
+app.use(flash());
+app.use(require('express-session')({
+    secret: env_vars.secretCode,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {_expires: 24 * 60 * 60 * 1000}
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session({}));
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+passport_config.config_passport(passport);
+// passport.use(new LocalStrategy())
+
+
+// ROUTES
+
+app.get("/", (req, res) => {
+    res.redirect("/dashboard");
+});
+
+app.use(user);
+
+//404
+app.use(function (req, res, next) {
+    res.status(404);
+    // respond with html page
+    res.render('error/error_404', { url: req.url,user: req.user });
+    // res.send("no");
+    return;
+});
+  
+
+const listenPort = process.env.PORT  || env_vars.PORT_NUMBER ;
+
+//server setup
+app.listen(listenPort, ()=>{
+    console.log('The server is running at :', listenPort);
+});
